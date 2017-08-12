d65922cfa33fc626334f68f3c418f16c7d966411
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-12 20:09:33 +0900

    first commit

---
+++ main.out/modern_cljs/core.js
@@ -0,0 +1,11 @@
+// Compiled by ClojureScript 1.7.228 {}
+goog.provide('modern_cljs.core');
+goog.require('cljs.core');
+modern_cljs.core.add = (function modern_cljs$core$add(x,y){
+return (x + y);
+});
+add = modern_cljs.core.add;
+cljs.core.enable_console_print_BANG_.call(null);
+cljs.core.println.call(null,"Hello world!");
+
+//# sourceMappingURL=core.js.map
